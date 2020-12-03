a277713db9afa947b470f214e8097a20b1c3ec11
Akshay Kanthi <aksanoble@gmail.com>
2020-12-04 00:08:52 +0530

    save incoming and outgoing messages

--- index.js
+++ index.js
@@ -1,27 +1,82 @@
-const { Telegraf } = require("telegraf");
 const express = require("express");
-const expressApp = express();
-const { upsertUser } = require("./queries");
-
+const { upsertUserQuery, insertMessageQuery } = require("./queries");
+const { fetchGQL } = require("./utils");
+const TelegramBot = require("node-telegram-bot-api");
+const to = require("await-to-js").default;
+const app = express();
 require("dotenv").config();
+const TOKEN = process.env.BOT_TOKEN;
+const url = process.env.WEBHOOK_PATH;
+const port = process.env.PORT || 3000;
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => {
-  const { id, first_name, username } = ctx.update.message.from;
-  upsertUser({ id, first_name, username });
-  // console.log(JSON.stringify(ctx.update), "ctx");
-  ctx.reply("Hello");
-});
-bot.help((ctx) => ctx.reply("Help message"));
-bot.on("message", (ctx) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message));
-bot.action("delete", ({ deleteMessage }) => deleteMessage());
-expressApp.use(bot.webhookCallback("/secret-path"));
-bot.telegram.setWebhook(process.env.WEBHOOK_PATH);
+const bot = new TelegramBot(TOKEN);
+app.use(express.json());
 
-expressApp.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post(`/secret-path`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
 });
 
-expressApp.listen(process.env.PORT || 3000, () => {
-  console.log("Example app listening on port 3000!");
+app.listen(port, () => {
+  console.log(`Express server is listening on ${port}`);
 });
+
+bot.setWebHook(`${url}`);
+
+bot.onText(/\/start/, async msg => {
+  const chatId = msg.chat.id;
+  const { id, first_name, username } = msg.from;
+  const [err, data] = await to(
+    fetchGQL.request(upsertUserQuery, { id, first_name, username: username || first_name })
+  );
+  if (err) {
+    console.log(err, "Error in saving user")
+    // saveAndSend(
+    //   chatId,
+    //   "There was an internal error, we're working on fixing it."
+    // );
+  } else {
+    saveAndSend(chatId, "Thank you for registering!");
+  }
+});
+
+bot.on("message", async msg => {
+  console.log("Incoming context", msg);
+  let [e, data] = await to(
+    fetchGQL.request(insertMessageQuery, {
+      id: msg.message_id,
+      timestamp: new Date(msg.date*1000).toISOString(),
+      user_id: msg.from.id,
+      text: msg.text,
+      chat_id: msg.chat.id
+    })
+  );
+
+  if (e) {
+    console.log("Error in saving message", e);
+  }
+  saveAndSend(msg.chat.id, "I am alive!").then(d => {
+    // console.log("Sent Context", d);
+  });
+});
+
+const saveAndSend = async (chatId, txt) => {
+  let [err, msg] = await to(bot.sendMessage(chatId, txt));
+  if (err) {
+    console.log("Error in delivering message to Telegram: ", msg, err);
+  }
+  let [e, data] = await to(
+    fetchGQL.request(insertMessageQuery, {
+      id: msg.message_id,
+      timestamp: new Date(msg.date*1000).toISOString(),
+      user_id: msg.from.id,
+      text: msg.text,
+      chat_id: msg.chat.id
+    })
+  );
+
+  if (e) {
+    console.log("Error in saving message", e);
+  }
+  return [msg, data];
+};
--- queries.js
+++ queries.js
@@ -1,9 +1,7 @@
-const { fetchGQL } = require("./utils");
 const { gql } = require("graphql-request");
-const to = require("await-to-js").default;
 
-const query = gql`
-  mutation MyMutation($username: String!, $first_name: String!, $id: Int!) {
+const upsertUserQuery = gql`
+  mutation UpsertUser($username: String!, $first_name: String!, $id: Int!) {
     insert_user(
       objects: { id: $id, username: $username, first_name: $first_name }
       on_conflict: { constraint: user_pkey, update_columns: username }
@@ -17,15 +15,31 @@ const query = gql`
   }
 `;
 
-const upsertUser = async (variables) => {
-  const [err, data] = await to(fetchGQL.request(query, variables));
-  if (err) {
-    console.log("Error in fetching Graphql", err);
-  } else {
-    console.log(JSON.stringify(data), "data");
+const insertMessageQuery = gql`
+  mutation InsertMessage($chat_id:Int!
+    $id:Int!
+    $text:String!
+    $user_id:Int!
+    $timestamp:timestamptz!) {
+    insert_message(
+      objects: {
+        chat_id:$chat_id
+        id:$id
+        text:$text
+        user_id:$user_id
+        timestamp:$timestamp
+      }
+    ) {
+      returning {
+        chat_id
+        id
+        text
+      }
+    }
   }
-};
+`;
 
 module.exports = {
-  upsertUser,
+  upsertUserQuery,
+  insertMessageQuery
 };
