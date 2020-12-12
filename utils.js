d4cc1aebbb5485e29f9139a17aa636b1c8d0c476
Akshay Kanthi <aksanoble@gmail.com>
2020-12-12 12:37:04 +0530

    wip

--- utils.js
+++ utils.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const { GraphQLClient } = require("graphql-request");
 
 const fetchGQL = new GraphQLClient(process.env.GRAPHQL_ENDPOINT);
+fetchGQL.setHeader("X-Hasura-Admin-Secret", process.env.HASURA_ADMIN_SECRET);
 
 module.exports = {
-  fetchGQL,
+  fetchGQL
 };
